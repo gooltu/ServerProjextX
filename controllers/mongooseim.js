3f11376b3e64b9cd03d486e99b83ecd85d043ca9
mayukh <49320647+raybabu@users.noreply.github.com>
2021-04-04 09:39:42 +0530

    upd

--- controllers/mongooseim.js
+++ controllers/mongooseim.js
@@ -6,11 +6,15 @@ let jwt = require('jsonwebtoken');
 
 let admin = require('firebase-admin');
 
+
+
+
+
 let serviceAccount = {
 	type: process.env.ftype,
   project_id: process.env.project_id,
   private_key_id: process.env.private_key_id,
-  private_key: process.env.private_key,
+  private_key: process.env.private_key.replace(/\\n/g, '\n'),
   client_email: process.env.client_email,
   client_id: process.env.client_id,
   auth_uri: process.env.auth_uri,
